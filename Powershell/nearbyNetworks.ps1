abb2ff64e69c8840643f3416efa7a8e1e953496a
Ainsley <ainsleycabading.21@ichat.sp.edu.sg>
2023-11-12 15:32:59 +0800

    Added some filtering

--- Powershell/nearbyNetworks.ps1
+++ Powershell/nearbyNetworks.ps1
@@ -1,5 +1,9 @@
 # display the nearby networks using Write-Host
 
+$nearbyNetworks = netsh wlan show networks mode=Bssid
+
 $filteredNetworks = $nearbyNetworks | Select-String "SSID", "Authentication", "Encryption"
 
+Write-Host "Script #7 Running..."
+Write-Host "List of Nearby Networks:"
 Write-Host $filteredNetworks
